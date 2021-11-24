241435ebab5119020b18ec7c1de69801bc20b9e6
kexin-Star <kexinl@umich.edu>
2021-11-23 22:05:23 -0500

    mobile_tablet view

---
+++ GuoBa_blog/javascript/index.js
@@ -0,0 +1,10 @@
+function openNav() {
+    console.log("open now");
+    document.querySelector(".nav_links").style.width = "150px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.querySelector(".nav_links").style.width = "0";
+  }
+
